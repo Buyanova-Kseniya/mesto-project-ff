1f94cea90f0493b83c9ff3f72e1575008b678bcc
Kseniya Buyanova <YuleCat25@yandex.ru>
2025-06-19 13:30:29 +0300

    /

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,17 +1,17 @@
 export function openModal(popupElement) {
-  popupElement.classList.add('popup_is-opened');
+  popupElement.classList.add("popup_is-opened");
 
   function hendlerEscape(evt) {
-    if (evt.key === 'Escape') {
+    if (evt.key === "Escape") {
       closeModal(popupElement);
     }
   }
-  document.addEventListener('keydown', hendlerEscape);
+  document.addEventListener("keydown", hendlerEscape);
   openModal.hendlerEsc = hendlerEscape;
 }
 
 export function closeModal(popupElement) {
-  popupElement.classList.remove('popup_is-opened');
-  document.removeEventListener('keydown', openModal.hendlerEsc);
+  popupElement.classList.remove("popup_is-opened");
+  document.removeEventListener("keydown", openModal.hendlerEsc);
   delete openModal.hendlerEsc;
 }
