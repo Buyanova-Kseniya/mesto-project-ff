d3b81903cd20553fc783f82c18322524111c2f61
Kseniya Buyanova <YuleCat25@yandex.ru>
2025-06-26 14:34:33 +0300

    Update modal.js

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,23 +1,18 @@
-let escapeHandler = null;
+const escapeHandler = function(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    if (openedPopup) {
+      closeModal(openedPopup);
+    }
+  }
+};
 
 export function openModal(popupElement) {
   popupElement.classList.add("popup_is-opened");
-  escapeHandler = function(evt) {
-    if (evt.key === "Escape") {
-      const openedPopup = document.querySelector('.popup_is-opened');
-      if (openedPopup) {
-        closeModal(openedPopup);
-      }
-    }
-  };
-
   document.addEventListener("keydown", escapeHandler);
 }
 
 export function closeModal(popupElement) {
   popupElement.classList.remove("popup_is-opened");
-  if (escapeHandler) {
-    document.removeEventListener("keydown", escapeHandler);
-    escapeHandler = null;
-  }
+  document.removeEventListener("keydown", escapeHandler);
 }
